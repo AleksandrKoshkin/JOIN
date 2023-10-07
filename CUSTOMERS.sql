b3703f079860f0f2605c9ea536ba774f8e7c9f32
Aleksandr <aleksandr.koshkin.2000@mail.ru>
2023-10-07 19:45:49 +0300

    Two

--- CUSTOMERS.sql
+++ CUSTOMERS.sql
@@ -4,5 +4,5 @@ id   int auto_increment primary key,
 name varchar(255),
 surname varchar(255),
 age int,
-phone_number int,
+phone_number int
 );
--- ORDERS.sql
+++ ORDERS.sql
@@ -3,5 +3,5 @@ id   int auto_increment primary key,
 date  int,
 customer_id int references customers (id),
 product_name varchar(255),
-amount int,
+amount int
 );
